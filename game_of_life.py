5f267da1c61a360ebb0280c1ae48c9e292d32a62
Derek <dckirwan07@gmail.com>
2022-05-09 16:54:56 -0400

    removed game_grid.py and moved its set_grid() function to game_cell.py and made it a method of GameCell. Enabled GameCells to keep track of their column and row position in the grid.

--- game_of_life.py
+++ game_of_life.py
@@ -3,7 +3,13 @@
 import tkinter as tk
 from tkinter.tix import COLUMN
 
-import game_grid as grd
+import game_cell as gc
+
+game_cell = gc.GameCell
+
+def start():
+        # print("test")
+        game_cell.get_neighbor_status()
 
 def program_start():
     """Greets user on start up and creates GUI main window and game grid, and sets all the 'cells' to 'dead'."""
@@ -13,17 +19,18 @@ def program_start():
     gui.title("Game of Life")
     gui.geometry("1920x1080")
     # Grid settings
-    grd.set_grid()
+    game_cell.set_grid()
     # Button settings
     seed_button = tk.Button(gui, text="Seed", width=25)
     seed_button.place(x=300, y=950)
-    start_button = tk.Button(gui, text="Start", width=25)
+    start_button = tk.Button(gui, text="Start", width=25, command=start)
     start_button.place(x=600, y=950)
     stop_button = tk.Button(gui, text="Stop", width=25)
     stop_button.place(x=900, y=950)
     quit_button = tk.Button(gui, text="Quit", width=25, command=gui.destroy)
     quit_button.place(x=1200, y=950)
     gui.mainloop()
+
 
 def main():
     running = True
