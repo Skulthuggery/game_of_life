8240f2458d185f77138410553b5e292cb58d8959
Derek <dckirwan07@gmail.com>
2022-05-12 20:58:10 -0400

    A lot of changes, should have done more smaller commits. But, right now that issue is that I can't get the program to determine when to loop around GameCell.get_neighbor_status() and GameCell.new_generation. What i want is for the game to be static at first, letting you click buttons to set up, and then when you click the start button, the game begins looping through those 2 methods and changing the board state continuously until the user clicks the start button. I can't figure this out.

--- game_cell.py
+++ game_cell.py
@@ -2,8 +2,6 @@
 
 import tkinter as tk
 
-game_grid_list = []
-game_grid_dict = {}
 
 class GameCell:
     """models the cells used in the Game of Life."""
@@ -12,9 +10,10 @@ class GameCell:
         self.color = color
         self.col_num = 0
         self.row_num = 0
+        self.alive_neighbors = 0
         self.button = tk.Button(bg=color, command=self.set_seed)
 
-    def set_grid():
+    def set_grid(game_grid_list, game_grid_dict):
         for num_c in range(66):
             for num_r in range(30):
                 game_cell = GameCell()
@@ -33,63 +32,66 @@ class GameCell:
             self.alive = False
             self.button["bg"] = "white"
 
-    def start_generations(self):
-        print("test")
-        # self.get_neighbor_status(self, game_grid_list, game_grid_dict)
-        # self.new_generation(self, alive_neighbors)
+    # def start_generations(running, game_cell):
+    #     if not running:
+    #         print("test")
+    #     elif running:
+    #         game_cell.get_neighbor_status(game_cell, game_grid_list, game_grid_dict)
+    #         game_cell.new_generation(game_cell, game_grid_list)
     
     def get_neighbor_status(self, game_grid_list, game_grid_dict):
         """For each cell in game_cell_list, checks its neighboring cells to determine if its alive then increments alive_neighbors if so, then calls new_generation() method"""
-        alive_neighbors = 0
-        for game_cell in game_grid_list:
+        for self in game_grid_list:
+            self.alive_neighbors = 0
             """North-West Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num - 1}/{game_cell.row_num - 1}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num - 1}/{self.row_num - 1}"] == True:
+                self.alive_neighbors += 1
             else: pass
             """North Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num}/{game_cell.row_num - 1}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num}/{self.row_num - 1}"] == True:
+                self.alive_neighbors += 1
             else: pass
             """North-East Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num + 1}/{game_cell.row_num - 1}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num + 1}/{self.row_num - 1}"] == True:
+                self.alive_neighbors += 1
             else: pass
             """West Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num - 1}/{game_cell.row_num}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num - 1}/{self.row_num}"] == True:
+                self.alive_neighbors += 1
             else: pass
             """East Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num + 1}/{game_cell.row_num}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num + 1}/{self.row_num}"] == True:
+                self.alive_neighbors += 1
             else: pass
             """South-West Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num - 1}/{game_cell.row_num + 1}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num - 1}/{self.row_num + 1}"] == True:
+                self.alive_neighbors += 1
             else: pass
             """South Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num}/{game_cell.row_num + 1}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num}/{self.row_num + 1}"] == True:
+                self.alive_neighbors += 1
             else: pass
             """South-East Neighbor"""
-            if game_grid_dict[f"{game_cell.col_num + 1}/{game_cell.row_num + 1}"] == True:
-                alive_neighbors += 1
+            if game_grid_dict[f"{self.col_num + 1}/{self.row_num + 1}"] == True:
+                self.alive_neighbors += 1
             else: pass
-        game_cell.new_generation(self, alive_neighbors)
 
-        def new_generation(self, alive_neighbors):
-            """Checks how many neighbors are alive, then sets the active cell to either 'alive' or 'dead' based on the number of alive neighbor cells.
-            The active cell's button color is changed appropriately."""
+    def new_generation(self, game_grid_list):
+        """Checks how many neighbors are alive, then sets the active cell to either 'alive' or 'dead' based on the number of alive neighbor cells.
+        The active cell's button color is changed appropriately."""
+        for self in game_grid_list:
             if self.alive == True:
-                if alive_neighbors <= 1:
+                if self.alive_neighbors <= 1:
                     self.alive = False
                     self.button["bg"] = "white"
-                elif alive_neighbors > 1 and alive_neighbors <= 3:
+                elif self.alive_neighbors > 1 and self.alive_neighbors <= 3:
                     pass
-                elif alive_neighbors >= 4:
+                elif self.alive_neighbors >= 4:
                     self.alive = False
                     self.button["bg"] = "white"
             elif self.alive == False:
-                if alive_neighbors == 3:
+                if self.alive_neighbors == 3:
                     self.alive = True
                     self.button["bg"] = "black"
-                else: pass
+                else: 
+                    pass
--- game_of_life.py
+++ game_of_life.py
@@ -4,29 +4,47 @@ import tkinter as tk
 
 import game_cell as gc
 
-game_cell = gc.GameCell
 
-def program_start():
+def start_running():
+    running = True
+
+
+def stop_running(): 
+    running = False
+
+def program_start(gui, game_cell, game_grid_list, game_grid_dict):
     """Greets user on start up and creates GUI main window and game grid, and sets all the 'cells' to 'dead'."""
-    print("\nWelcome to the Game of Life!\n")
-    # Window settings
-    gui = tk.Tk()
+    set_up_window(gui)
+    set_up_buttons(gui)
+    game_cell.set_grid(game_grid_list, game_grid_dict)
+    # game_cell.start_generations(running, game_cell)
+
+def set_up_window(gui):
     gui.title("Game of Life")
     gui.geometry("1920x1080")
-    # Main Program
-    game_cell.set_grid()
-    # Button settings
-    start_button = tk.Button(gui, text="Start", width=25, command=lambda: game_cell.start_generations)
+
+def set_up_buttons(gui):
+    start_button = tk.Button(gui, text="Start", width=25, command=start_running)
     start_button.place(x=600, y=950)
-    stop_button = tk.Button(gui, text="Stop", width=25)
+    stop_button = tk.Button(gui, text="Stop", width=25, command=stop_running)
     stop_button.place(x=900, y=950)
     quit_button = tk.Button(gui, text="Quit", width=25, command=gui.destroy)
     quit_button.place(x=1200, y=950)
-    gui.mainloop()
-
 
 def main():
-    program_start()
+    gui = tk.Tk()
+    running = False
+    game_cell = gc.GameCell
+    game_grid_list = []
+    game_grid_dict = {}
+    program_start(gui, game_cell, game_grid_list, game_grid_dict)
+    # if not running:
+    #     pass
+    # else:
+    game_cell.get_neighbor_status(game_cell, game_grid_list, game_grid_dict)
+    game_cell.new_generation(game_cell, game_grid_list)
+
+    gui.mainloop()
 
 if __name__ == "__main__":
     main()
